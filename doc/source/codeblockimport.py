d02d692eead1504c9bef6cc9c6dd318f858d5c19
Mikhail Rybalkin <rybalkin@ggasoftware.com>
2013-06-26 21:59:18 +0400

    indigo-doc: code block can include other code blocks

--- doc/source/codeblockimport.py
+++ doc/source/codeblockimport.py
@@ -13,13 +13,24 @@ class CodeDirective(CodeBlock):
     optional_arguments = 0
 
     option_spec = dict(
-        name = str
+        name = str,
+        includecode = str
     )
 
     def run(self):
         global codeDict
         code = u'\n'.join(self.content)
-        codeDict[self.options['name']] = code
+        
+        codename = self.options['name']
+        
+        included_code = ""
+        if 'includecode' in self.options:
+            for name in self.options['includecode'].split(','):
+                included_code += codeDict[name] + "\n"
+        
+        if codename in codeDict:
+            raise RuntimeError("Code with name " + codename + " has already been defined")
+        codeDict[codename] = included_code + "\n\n" + code
         nodeList = []
         literal = nodes.literal_block(code, code)
         literal['language'] = 'python'
