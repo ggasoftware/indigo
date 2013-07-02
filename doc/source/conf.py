7ea066e8a867323e4ca3fb2716be03ec730cfecf
Mikhail Rybalkin <rybalkin@ggasoftware.com>
2013-07-02 21:09:28 +0400

    indigo-doc: copyright update

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Indigo'
-copyright = u'2012, GGA Software Services'
+copyright = u'2013, GGA Software Services'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
